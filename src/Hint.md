ca55b955f204a9c5b71c89f470fc61f00a963775
Basavaraj K N <rajiff@gmail.com>
2014-11-01 15:33:48 +0530

    added for assessment

---
+++ src/Hint.md
@@ -0,0 +1,1 @@
+# Project Hint for Students
---
+++ src/main/java/com/manipal/Exercise.java
@@ -0,0 +1,17 @@
+package com.manipal;
+
+/**
+ * Main exercise class of the Assessment
+ *
+ */
+public class Exercise {
+    /**
+    * Main method
+    * @param args 
+    */
+    public static void main(String[] args) {
+    	/* Code your solution here */
+
+    	System.out.println("<output>");
+    }
+}
